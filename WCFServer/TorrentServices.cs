016b0e99ce1dd9e690d8822adc6d51e95e5613e7
Michael <sabbah49@gmail.com>
2019-02-21 20:13:54 +0200

    Initial commit

---
+++ WCFServer/TorrentServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Services" in both code and config file together.
+    public class TorrentServices : ITorrentServices
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
